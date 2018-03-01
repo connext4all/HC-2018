1215a65b96b07e6b4cceb5950e23dfd2b25063cf
Pedro Torres <pedrotorres@mbp-de-pedro.home>
2018-03-01 22:27:02 +0100

    Final code

--- main.py
+++ main.py
@@ -7,15 +7,36 @@ class maps:
 
 		file = open(filename, "r")
 		lines = file.read().splitlines()
-		values = lines[0]
-		#print lines;
+		values = lines[0].split(" ")
+
 		self.rows 	  = values[0]
 		self.columns  = values[1]
 		self.n_veh	  = values[2]
 		self.n_rides  = values[3]
 		self.bonus	  = values[4]
-		self.map_steps= values[5]
-		self.rides 	  = rides(lines[1:])
+		self.map_steps= int(values[5])
+		self.rides_ 	  = rides(lines[1:])
+		self.vehicles_list = []
+		for x in range(int(self.n_veh)):
+			v = vehicle(id_v=(x+1))
+			self.vehicles_list.append(v)
+
+
+	def assign_ride(self,v):
+		dist_min = 99999999
+		id_ride_min = None
+		pos_del = None
+		for pos in self.rides_.rides_list:
+			dist = calc_distance(v.pos,pos["init_pos"])
+			if dist < dist_min:
+				dist_min = dist
+				id_ride_min = pos["id_ride"]
+				pos_del = pos
+		if id_ride_min is not None: v.assigned_rides.append(id_ride_min)
+		v.steps = dist_min
+
+		if pos_del is not None: self.rides_.rides_list.remove(pos_del)
+		
 	
 class rides:
 	def __init__(self,lines):
@@ -23,8 +44,9 @@ class rides:
 		i=0
 		for line in lines:
 			line = line.split(" ")
-			ride_params = {"init_pos":(line[0],line[1]),"finish_pos":(line[2],line[3]),"first_step":line[4],"deadline":line[5]}
+			ride_params = {"id_ride":i,"init_pos":(int(line[0]),int(line[1])),"finish_pos":(int(line[2]),int(line[3])),"first_step":int(line[4]),"deadline":int(line[5])}
 			self.rides_list.append(ride_params)
+			i = i+1
 
 	def print_list(self):
 		print self.rides_list
@@ -34,9 +56,9 @@ class vehicle:
 		self.id = id_v
 		self.trips = []
 		self.pos = pos
-		self.steps = 9
+		self.steps = 0
 		self.finish_pos = (0,0)
-
+		self.assigned_rides = []
 	def set_pos(self,pos=(0,0)):
 		self.pos = pos
 
@@ -45,17 +67,35 @@ class vehicle:
 
 
 def calc_distance(init_pos=(0,0), finish_pos=(0,0)):
-	a,b = self.init_pos
-	x,y = self.finish_pos
+	a,b = init_pos
+	x,y = finish_pos
 	return abs(a-x) + abs(b-y)
 
 
 if __name__ == "__main__":
 	
 	#file_w = open("output/salida1","w")
-	map = maps()
+	map_ = maps("datasets/e_high_bonus.in")
+	file_w = open("output/output_e.txt","w") 
+
+	for i in xrange(0,map_.map_steps):
+		for v in map_.vehicles_list:
+			if v.steps == 0:
+				map_.assign_ride(v)
+			v.steps=v.steps-1
 	
-	#for pos in map.rides.rides_list:
+	for v in map_.vehicles_list:
+		print str(v.id) + str(v.assigned_rides)
+		file_w.write(str(v.id))
+		for a in v.assigned_rides:
+			file_w.write(" " + str(a))
+		file_w.write("\n")
+
+		
+
+
+
+
 		 
 
 
